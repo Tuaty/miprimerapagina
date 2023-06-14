aceb393034a1078c72195bda7d9e51aab5f794d1
Tuaty <nathalytuaty.inn@gmail.com>
2023-06-13 22:18:22 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function showalert(){alert ("¡Hola, esta es una alerta desde JavaScript!");}
+
+document.querySelector("button.button-menu-toggle")
+
+    .addEventListener("click", function() {
+
+           document.querySelector(".nav-links").
+
+                      classList.toggle("nav-links-responsive")})
